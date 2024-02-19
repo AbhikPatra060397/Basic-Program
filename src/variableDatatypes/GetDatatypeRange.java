deae2bdec8882fb3628c2659bd6e935e9646df85
AbhikPatra060397 <abhik.patra2021@gmail.com>
2024-02-19 21:46:00 +0530

    GetDatatypeRange : Data types range Minimum to Maximum

---
+++ src/variableDatatypes/GetDatatypeRange.java
@@ -0,0 +1,13 @@
+package variableDatatypes;
+
+public class GetDatatypeRange {
+    public static void main(String[] args) {
+        System.out.println("range of byte :  ( "+ Byte.MIN_VALUE + " to " + Byte.MAX_VALUE + " )");
+        System.out.println("range of short :  ( "+ Short.MIN_VALUE + " to " + Short.MAX_VALUE + " )");
+        System.out.println("range of int :  ( "+ Integer.MIN_VALUE + " to " + Integer.MAX_VALUE + " )");
+        System.out.println("range of char :  ( "+ Character.MIN_VALUE + " to " + Character.MAX_VALUE + " )");
+        System.out.println("range of float :  ( "+ Float.MIN_VALUE + " to " + Float.MAX_VALUE + " )");
+        System.out.println("range of double :  ( "+ Double.MIN_VALUE + " to " + Double.MAX_VALUE + " )");
+        System.out.println("range of long :  ( "+ Long.MIN_VALUE + " to " + Long.MAX_VALUE + " )");
+    }
+}
